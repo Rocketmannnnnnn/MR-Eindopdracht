4e92d60cfb21ff28f97d3cc8049bf9bf6661bbc3
Jasper <j.bonefaas@student.avans.nl>
2021-03-15 13:34:05 +0100

    Added custom gravity for bridge

---
+++ MR-Eindopdracht/Assets/Scripts/Physics/CustomGravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CustomGravity : MonoBehaviour
+{
+    List<Rigidbody> bodies;
+    public Vector3 gravityConstant = new Vector3(0, -9.81f, 0);
+
+    private void Awake()
+    {
+        bodies = new List<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.attachedRigidbody;
+        bodies.Add(rb);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rb = other.attachedRigidbody;
+        bodies.Remove(rb);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach(Rigidbody body in this.bodies)
+        {
+            body.AddForce(body.mass * gravityConstant);
+        }
+    }
+}
