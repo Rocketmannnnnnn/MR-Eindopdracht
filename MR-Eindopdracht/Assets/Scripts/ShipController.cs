21900c22705e126f71359d19ab0fffead1dfb24d
Jasper <j.bonefaas@student.avans.nl>
2021-03-22 12:31:24 +0100

    Added spaceship controller

---
+++ MR-Eindopdracht/Assets/Scripts/ShipController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IShipController
+{
+    void Move(Vector3 movement);
+    void Rotate(Vector3 rotation);
+}
+
+public class ShipController : MonoBehaviour, IShipController
+{
+    private Rigidbody rb;
+    public float movementSpeed = 1.0f;
+    public float rotationSpeed = 1.0f;
+
+    private void Awake()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 movement)
+    {
+        Vector3 move = new Vector3(0,0,0);
+        move += movement.x * transform.right;
+        move += movement.y * transform.up;
+        move += movement.z * transform.forward;
+        rb.MovePosition(transform.position + move * this.movementSpeed);
+    }
+
+    public void Rotate(Vector3 rotation)
+    {
+        Vector3 rot = new Vector3(0,0,0);
+        rot.x += rotation.x * this.rotationSpeed;
+        rot.y += rotation.y * this.rotationSpeed;
+        rot.z += rotation.z * this.rotationSpeed;
+        transform.Rotate(rot);
+    }
+}
---
+++ MR-Eindopdracht/Assets/Scripts/TestScripts/ShipControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControllerTest : MonoBehaviour
+{
+    public IShipController controller;
+    public string spaceshipTag = "Spaceship";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject spaceship = GameObject.FindGameObjectWithTag(this.spaceshipTag);
+        this.controller = spaceship.GetComponent<IShipController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3();
+        movement.x = Input.GetKey(KeyCode.D) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.A) ? -Time.fixedDeltaTime : 0;
+        movement.y = Input.GetKey(KeyCode.E) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.Q) ? -Time.fixedDeltaTime : 0;
+        movement.z = Input.GetKey(KeyCode.W) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.S) ? -Time.fixedDeltaTime : 0;
+        this.controller.Move(movement);
+
+        Vector3 rotation = new Vector3();
+        rotation.x = Input.GetKey(KeyCode.I) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.K) ? -Time.fixedDeltaTime : 0;
+        rotation.y = Input.GetKey(KeyCode.L) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.J) ? -Time.fixedDeltaTime : 0;
+        rotation.z = Input.GetKey(KeyCode.U) ? Time.fixedDeltaTime : Input.GetKey(KeyCode.O) ? -Time.fixedDeltaTime : 0;
+        this.controller.Rotate(rotation);
+    }
+}
