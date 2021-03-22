1964f8a3d1dcf26dafe2717e38119bf3771cc969
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2021-03-22 12:43:34 +0100

    Added Key only test code for a sphere and lever controll

---
+++ MR-Eindopdracht/Assets/Scripts/Control/ControlBall.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBall : MonoBehaviour
+{
+
+    public int scale;
+
+    public float readRotationSpeed;
+    
+    private Vector3 rotation;
+    private Vector3 original;
+
+    private GameObject controlBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.controlBall = GameObject.Find("Test-control-ball");
+        this.original = this.controlBall.transform.position;
+
+        Debug.Log("test!");
+
+        StartCoroutine("checkBallPosition");
+    }
+
+    private IEnumerable checkBallPosition()
+    {
+
+        while (true)
+        {
+
+            this.rotation = new Vector3(0, 0, 0);
+
+            if (Input.GetKeyDown(KeyCode.A)) this.rotation.x -= 1;
+            if (Input.GetKeyDown(KeyCode.D)) this.rotation.x += 1;
+            if (Input.GetKeyDown(KeyCode.S)) this.rotation.y -= 1;
+            if (Input.GetKeyDown(KeyCode.W)) this.rotation.y += 1;
+            if (Input.GetKeyDown(KeyCode.Q)) this.rotation.z -= 1;
+            if (Input.GetKeyDown(KeyCode.E)) this.rotation.z += 1;
+
+            this.controlBall.transform.position = (this.original + this.rotation);
+
+            Debug.Log(this.rotation);
+
+            yield return new WaitForSeconds(this.readRotationSpeed);
+        }
+    }
+}
---
+++ MR-Eindopdracht/Assets/Scripts/Control/ControlLever.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLever : MonoBehaviour
+{
+
+    public int readLeverspeed;
+
+    private bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.isActive = false;
+
+        Debug.Log("testLever");
+
+        StartCoroutine("checkLeverPulled");
+    }
+
+    IEnumerable checkLeverPulled()
+    {
+
+        while (true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space)) this.isActive = !this.isActive;
+
+            Debug.Log(this.isActive);
+
+            yield return new WaitForSeconds(this.readLeverspeed);
+        }
+    }
+}
