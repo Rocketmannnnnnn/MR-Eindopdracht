01d54436fb669cc553d8e5b4fb21e3819824c769
Jasper <j.bonefaas@student.avans.nl>
2021-03-23 20:59:45 +0100

    Added lever and joystick

---
+++ MR-Eindopdracht/Assets/Scripts/Input/ConfigJointAxis.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigJointAxis : InputComponent
+{
+    public ConfigurableJoint joint;
+    
+    [System.Serializable]
+    public enum Axis { X, Y, Z}
+    public Axis axis;
+    
+    //Returns the angle in a range between -1 and 1
+    public override float GetNormalizedAxis()
+    {
+        switch (this.axis)
+        {
+            case Axis.X:
+                return Mathf.InverseLerp(this.joint.lowAngularXLimit.limit, this.joint.highAngularXLimit.limit, GetJointRotation(this.joint).eulerAngles.x) * 2.0f - 1.0f;
+            case Axis.Y:
+                return Mathf.InverseLerp(-this.joint.angularYLimit.limit, this.joint.angularYLimit.limit, GetJointRotation(this.joint).eulerAngles.y) * 2.0f - 1.0f;
+            default:
+                return Mathf.InverseLerp(-this.joint.angularZLimit.limit, this.joint.angularZLimit.limit, GetJointRotation(this.joint).eulerAngles.z) * 2.0f - 1.0f;
+        }
+    }
+
+    public Quaternion GetJointRotation(ConfigurableJoint joint)
+    {
+        return (Quaternion.FromToRotation(joint.axis, joint.connectedBody.transform.rotation.eulerAngles));
+    }
+}
---
+++ MR-Eindopdracht/Assets/Scripts/Input/Lever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Lever : InputComponent
+{
+    private CircularDrive circularDrive;
+    public float minAngle = -60;
+    public float maxAngle = 60;
+    
+    void Start()
+    {
+        this.circularDrive = GetComponent<CircularDrive>();
+        this.circularDrive.minAngle = this.minAngle;
+        this.circularDrive.maxAngle = this.maxAngle;
+    }
+
+    //Returns the angle in a range between -1 and 1
+    public override float GetNormalizedAxis()
+    {
+        return Mathf.InverseLerp(this.minAngle, this.maxAngle, this.circularDrive.outAngle) * 2.0f - 1.0f;
+    }
+}
