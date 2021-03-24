f44b86c60371ee2678d8ed76b7857274f2a3b0db
Jasper <j.bonefaas@student.avans.nl>
2021-03-24 15:46:56 +0100

    Added InputBridge between controls and spaceship

--- MR-Eindopdracht/Assets/Scripts/Input/ConfigJointAxis.cs
+++ MR-Eindopdracht/Assets/Scripts/Input/ConfigJointAxis.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 public class ConfigJointAxis : InputComponent
 {
     public ConfigurableJoint joint;
+    public bool invert = false;
     
     [System.Serializable]
-    public enum Axis { X, Y, Z}
+    public enum Axis { X, Z}
     public Axis axis;
     
     //Returns the angle in a range between -1 and 1
     public override float GetNormalizedAxis()
     {
-        switch (this.axis)
-        {
-            case Axis.X:
-                return Mathf.InverseLerp(this.joint.lowAngularXLimit.limit, this.joint.highAngularXLimit.limit, GetJointRotation(this.joint).eulerAngles.x) * 2.0f - 1.0f;
-            case Axis.Y:
-                return Mathf.InverseLerp(-this.joint.angularYLimit.limit, this.joint.angularYLimit.limit, GetJointRotation(this.joint).eulerAngles.y) * 2.0f - 1.0f;
-            default:
-                return Mathf.InverseLerp(-this.joint.angularZLimit.limit, this.joint.angularZLimit.limit, GetJointRotation(this.joint).eulerAngles.z) * 2.0f - 1.0f;
-        }
+        float normalized = 0;
+        if (axis == Axis.X)
+            normalized = Mathf.InverseLerp(this.joint.lowAngularXLimit.limit, this.joint.highAngularXLimit.limit, GetJointAngle());
+        else
+            normalized = Mathf.InverseLerp(-this.joint.angularZLimit.limit, this.joint.angularZLimit.limit, GetJointAngle());
+
+        normalized = normalized * 2.0f - 1.0f;
+
+        if (this.invert)
+            normalized *= -1;
+
+        return normalized;
     }
 
-    public Quaternion GetJointRotation(ConfigurableJoint joint)
+    public float GetJointAngle()
     {
-        return (Quaternion.FromToRotation(joint.axis, joint.connectedBody.transform.rotation.eulerAngles));
+        float deltaMove;
+        if(this.axis == Axis.X)
+        {
+            deltaMove = this.joint.connectedBody.transform.localPosition.x;
+        }
+        else
+        {
+            deltaMove = this.joint.connectedBody.transform.localPosition.z;
+        }
+
+        float angle = Mathf.Atan2(this.joint.connectedBody.transform.localPosition.y, deltaMove) * Mathf.Rad2Deg;
+        angle -= 90;
+        return angle;
     }
 }
