f0fb926c65f6b0c4cc395446b1da0e5ea55ac15d
Jasper <j.bonefaas@student.avans.nl>
2021-03-28 14:47:41 +0100

    Added planets, comets and asteroids

---
+++ MR-Eindopdracht/Assets/Scripts/Comet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comet : MonoBehaviour
+{
+    public float force = 100;
+    
+    private void Awake()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        Vector3 rotation = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+        transform.rotation = Quaternion.Euler(rotation);
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
