f7da9063ecdd62921c5e2ebafb6035b212dac5a3
Quincy <q.baljeu@gmail.com>
2021-03-29 21:02:08 +0200

    Added color changing to script

--- MR-Eindopdracht/Assets/ButtonEvent.cs
+++ MR-Eindopdracht/Assets/ButtonEvent.cs
@@ -6,10 +6,30 @@ using Valve.VR.InteractionSystem;
 public class ButtonEvent : MonoBehaviour
 {
     public TurretControl turretControl;
+    private Renderer renderer;
 
-   public void onPress(Hand hand)
-   {
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+
+    private void Update()
+    {
+        if (turretControl.CanFire)
+        {
+            renderer.material.color = Color.green;
+        }
+        else
+        {
+            renderer.material.color = Color.red;
+        }
+    }
+
+    public void onPress(Hand hand)
+    {
         turretControl.ToggleCanFire();
         Debug.Log("SteamVR hover button was pressed");
-   }
+    }
+
 }
